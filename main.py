9051734e8634ecb0829eb4b172597b7f250b7aa9
“Jon <jonkyleester28@gmail.com>
2024-09-18 18:30:25 -0400

    fix name

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # main.py
 def hello_world():
-    return "Welcome to Git!"
+    return "Hello from Jon Ester!"
 
 if __name__ == "__main__":
     print(hello_world())
 
 def greet_person(name):
     return "Hello, [name]!"
+    
