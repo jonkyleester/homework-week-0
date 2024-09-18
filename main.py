2b5de835c9c1be12fd23c8f2fc7fe4d6062c7383
“Jon <jonkyleester28@gmail.com>
2024-09-18 18:22:19 -0400

    updated main and test

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ def hello_world():
 
 if __name__ == "__main__":
     print(hello_world())
+
+def greet_person(name):
+    return "Hello, [name]!"
--- test_main.py
+++ test_main.py
@@ -1,10 +1,17 @@
 # test_main.py
 import unittest
-from main import hello_world
+from main import greet_person, hello_world 
 
 class TestHelloWorld(unittest.TestCase):
+
+
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello from Jon Ester!")
 
+    def test_greet_person(self):
+        name = "Jon"
+        self.assertEqual(greet_person(name), "Hello, [name]!")
+
 if __name__ == '__main__':
     unittest.main()
+
