73b3941020c01ccab570dd94925487ad991904ff
“Jon <jonkyleester28@gmail.com>
2024-09-18 17:49:52 -0400

    Update Hello git

---
+++ main.py
@@ -0,0 +1,6 @@
+# main.py
+def hello_world():
+    return "Hello from Jon Ester!"
+
+if __name__ == "__main__":
+    print(hello_world())
---
+++ test_main.py
@@ -0,0 +1,10 @@
+# test_main.py
+import unittest
+from main import hello_world
+
+class TestHelloWorld(unittest.TestCase):
+    def test_hello_world(self):
+        self.assertEqual(hello_world(), "Hello from Jon Ester!")
+
+if __name__ == '__main__':
+    unittest.main()
